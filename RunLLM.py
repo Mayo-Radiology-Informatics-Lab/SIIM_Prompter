2f48274aae81487e13e057488d457aa706ddaf77
Bradley Erickson <bje@mayo.edu>
2024-06-10 17:04:08 -0500

    added lmstudio support

--- RunLLM.py
+++ RunLLM.py
@@ -1,18 +1,18 @@
 from radprompter import Prompt, RadPrompter, vLLMClient, OllamaClient, OpenAIClient
-import pandas as pd
-import numpy as np 
 import os
 import pandas as pd
-import argparse
-
+import pandas as pd
+import numpy as np 
+import pandas as pd
 
 if __name__ == '__main__':
-    
-        # Set up command line argument parsing
+    import argparse
+
+    # Set up command line argument parsing
     parser = argparse.ArgumentParser(description="Run LLM model inference")
     parser.add_argument('--model', type=str, default='llama3', help='Model name to use')
-    parser.add_argument('--server_port', type=int, default=11434, help='Server port number')
-    parser.add_argument('--interval', type=int, default=0, help='Interval for processing every <interval> reports')
+    parser.add_argument('--server_port', type=int, default=1234, help='Server port number')
+    parser.add_argument('--interval', type=int, default=10, help='Interval for processing and negative value skips inference altogether')
 
     # Parse arguments
     args = parser.parse_args()
@@ -22,9 +22,11 @@ if __name__ == '__main__':
     INTERVAL = args.interval
     
     prompt = Prompt("SIIM.toml")
+#    MODEL = 'mixtral:8x22b'
     INPUT_FILE = '~/Desktop/SIIMCombinedReports.xlsx'
     OUTPUT_FILE = '~/Desktop/SIIM_Results-' + MODEL + '.csv'
     TEMP_OUT = '~/Desktop/output-' + MODEL + '.csv'
+    
     
     if SERVER_PORT == 10000:
         # use this command to port-forward from a server (roqril0006a) running vllm which will then appear on localhost port 10000:
@@ -35,12 +37,20 @@ if __name__ == '__main__':
             temperature=0.0,
             seed=42
         )
-        print ("Using vLLM on port 10000 with model " + MODEL)
     elif SERVER_PORT == 11434:
         client = OllamaClient(
             model=MODEL,
             base_url="http://localhost:11434/v1",
+            api_key="ollama",
             temperature=0.0,
+            seed=42,
+        )
+    elif SERVER_PORT == 1234:
+        client = OpenAIClient(
+            model=MODEL,
+            base_url="http://localhost:1234/v1",
+            temperature=0.0,
+            api_key="lm-studio",
             seed=42,
         )
     elif SERVER_PORT == 0:
@@ -51,11 +61,13 @@ if __name__ == '__main__':
         seed=42
     )
     else:
-        print ("Error--unknown server port")
+        print (f"Error--unknown server port {SERVER_PORT}")
         exit(-1)
     print (f"Using port {SERVER_PORT} with model {MODEL}. output will be {OUTPUT_FILE}")
-        
+    
+    
     # delete any prior output
+    
     if os.path.exists(TEMP_OUT):
         os.remove(TEMP_OUT)
     
@@ -71,9 +83,12 @@ if __name__ == '__main__':
     
     # now read in the reports from SIIMCombinedReports.xlsx into a pandas dataframe
     
-    
     # Load the Excel file into a DataFrame
     reports_df = pd.read_excel(INPUT_FILE)
+    
+    if INTERVAL > 1:
+        reports_df = reports_df.iloc[::INTERVAL]
+        
     # strip spaces out of the FIndings column
     reports_df['Findings'] = reports_df['Findings'].str.replace(' ', '')
     # Remove any instance of '_x000D_' in the entire dataframe
@@ -133,10 +148,11 @@ if __name__ == '__main__':
     
     #---
     
-    
-    print ('Doing inference...')
-    out=engine(reports)
-    
+    if INTERVAL > -1:
+        print ('Doing inference...')
+        out=engine(reports)
+    else:
+        print ("Skipped doing inference")    
     #---
     
     
@@ -152,7 +168,6 @@ if __name__ == '__main__':
     out_df
     
     #---
-    
     
     if os.path.exists(OUTPUT_FILE):
         os.remove(OUTPUT_FILE)
